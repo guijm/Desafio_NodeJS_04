2e30a360a149a899310de659dff9bc7d5036ecbb
Guilherme Moraes <guilhermejm8@gmail.com>
2022-03-23 02:04:38 -0300

    desafio resolvido, usado array.map e reduce para a soma das notas

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,23 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+	class Aluno {
+		nome:string
+		idade:number
+		nota:number
+
+		constructor(nome:string,idade:number,nota:number){
+			this.nome = nome;
+			this.idade = idade;
+			this.nota = nota;
+		}
+	}
+	let listaAlunos:{aluno:Aluno}[]=[
+		{"aluno": new Aluno ("Aluno1", 20, 7)},
+		{"aluno": new Aluno ("Aluno2", 30, 7)},
+		{"aluno": new Aluno ("Aluno3", 34, 7)}
+	]
+     let somaNotas = listaAlunos.map(Aluno => Aluno.aluno.nota).reduce((prev,curr) => prev + curr, 0);
+	 console.log("O total das notas dos Alunos é: ",somaNotas);
+	 
 });
